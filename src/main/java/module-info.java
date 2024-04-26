a00b3e51f19acce07f1884c4bc9c37545e619000
analuciaguerrero <157589298+analuciaguerrero@users.noreply.github.com>
2024-04-27 01:13:58 +0200

    ya estan los controllers

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,8 @@ module com.example.demojavafx {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
+    requires java.logging;
 
 
     opens com.example.demojavafx to javafx.fxml;
