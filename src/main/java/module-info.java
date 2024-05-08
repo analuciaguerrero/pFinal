cb040ff18bae03b6d017d9f30ab83a3dc5f1aee7
Ivan <157744616+ivanalcazar04@users.noreply.github.com>
2024-05-08 11:54:23 +0200

    resuelto por profe test

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,7 @@ module com.example.demojavafx {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
+
     requires java.desktop;
     requires java.logging;
     requires com.google.gson;
